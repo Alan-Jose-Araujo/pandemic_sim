d904ec50b853105747a11bede628ea48cdd37210
Alan José <alanjsdelima@gmail.com>
2025-11-10 21:00:38 -0300

    Adding partial ImageGenerator class

---
+++ include/image_generator/ImageGenerator.hpp
@@ -0,0 +1,16 @@
+/**
+ * ImageGenerator.hpp
+ * @author Alan José <alanjsdelima@gmail.com>
+ *
+ * The ImageGenerator creates the visual example images.
+ */
+
+#ifndef IMAGE_GENERATOR_H
+#define IMAGE_GENERATOR_H
+
+namespace image_generator
+{
+    
+};
+
+#endif
