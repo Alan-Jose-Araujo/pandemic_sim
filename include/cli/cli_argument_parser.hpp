e574b5abef8581ba994667e5f8cb11ba38dab9e2
Alan José <alanjsdelima@gmail.com>
2026-01-26 17:52:21 -0300

    Add CLI argument parser implementation with option handling

---
+++ include/cli/cli_argument_parser.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file cli_argument_parser.hpp
+ * @brief A simple cli argument parser.
+ * @author Alan José <alanjsdelima@gmail.com>
+ * @date 2026-01-26
+ */
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include <map>
+#include <getopt.h>
+
+namespace Cli
+{
+    using Callback = std::function<void(const std::string &)>;
+
+    enum class CliArgumentType
+    {
+        Required = required_argument,
+        Optional = optional_argument,
+        None = no_argument
+    };
+
+    struct CliArgumentData
+    {
+        std::string longName;
+        bool hasValue;
+        Callback callback;
+    };
+
+    class CliArgumentParser
+    {
+    private:
+        std::map<char, CliArgumentData> options;
+
+    public:
+        void addOption(char shortCode, const std::string &longName, bool hasValue, Callback callback);
+
+        void parse(int argc, char *argv[]);
+    };
+};
